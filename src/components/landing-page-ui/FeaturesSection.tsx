7550eecdd5f327ada14c4615f36625fc5bac03a2
Jatin Kaushik <jkprogrammer25@gmail.com>
2025-08-02 17:48:15 +0530

    improve feature card ui

--- src/components/landing-page-ui/FeaturesSection.tsx
+++ src/components/landing-page-ui/FeaturesSection.tsx
@@ -1,36 +1,55 @@
+import { BookOpenTextIcon, MessageCircleMore, PencilIcon } from "lucide-react"
+
+const features = [
+  {
+    title: "Learn at Your Own Pace",
+    description:
+      "Whether you're a beginner or an advanced coder, AlgoMentor tailors the learning experience to your needs.",
+    icon: BookOpenTextIcon,
+    color: "teal"
+  },
+  {
+    title: "Instant Feedback",
+    description:
+      "Get immediate insights on your progress and understand where you can improve.",
+    icon: MessageCircleMore,
+    color: "blue"
+  },
+  {
+    title: "Hands-on Practice",
+    description:
+      "Solve real-world problems and get guided solutions that will help you crack the toughest interviews.",
+    icon: PencilIcon,
+    color: "amber"
+  }
+]
 
 const FeaturesSection = () => {
   return (
-    <section id="features" className="py-8 my-10 text-center">
-            <h2 className="text-4xl font-semibold">Why Choose AlgoMentor?</h2>
-            <p className="text-lg mt-4">
+    <section id="features" className="py-8 my-10">
+            <h2 className="text-4xl font-semibold text-center">Why Choose AlgoMentor?</h2>
+            <p className="text-lg mt-4 text-center">
               Unlock your coding potential with personalized, AI-driven
               insights.
             </p>
-            <div className="grid grid-cols-1 sm:grid-cols-3 gap-12 mt-20">
-              <div>
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-20">
+              {features.map((feature, index) => (
+                <div
+                  key={index}
+                  className={`flex flex-col items-center text-center p-6 rounded-lg shadow-md`}
+                >
+                <div className={`flex items-center justify-center w-min aspect-square p-3 mb-6 rounded-full bg-${feature.color}-500 shadow-lg shadow-${feature.color}-500/50`}>
+                  <feature.icon size={32} className="text-white" />
+                </div>
                 <h3 className="text-xl font-semibold">
-                  Learn at Your Own Pace
+                  {feature.title}
                 </h3>
-                <p className="mt-2 ">
-                  Whether you&apos;re a beginner or an advanced coder,
-                  AlgoMentor tailors the learning experience to your needs.
+                <div className={`w-20 h-[2px] bg-${feature.color}-500 rounded-full my-4 shadow-md shadow-${feature.color}-500`} />
+                <p className="mt-2 text-gray-600 dark:text-gray-400">
+                  {feature.description}
                 </p>
               </div>
-              <div>
-                <h3 className="text-xl font-semibold">Instant Feedback</h3>
-                <p className="mt-2 ">
-                  Get immediate insights on your progress and understand where
-                  you can improve.
-                </p>
-              </div>
-              <div>
-                <h3 className="text-xl font-semibold">Hands-on Practice</h3>
-                <p className="mt-2 ">
-                  Solve real-world problems and get guided solutions that will
-                  help you crack the toughest interviews.
-                </p>
-              </div>
+              ))}
             </div>
           </section>
   )
