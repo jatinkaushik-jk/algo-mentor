e1a9de308c1dc2a41200bcb35e218b974666c4e5
Jatin Kaushik <jkprogrammer25@gmail.com>
2025-08-18 22:08:48 +0530

    update theme changing logic

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import * as React from "react";
+import React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
@@ -14,7 +14,16 @@ import {
 
 export function ModeToggle({className}:{className?: string}) {
   const { setTheme } = useTheme();
-
+  const setUITheme = (theme: "light" | "dark" | "system") => {
+    setTheme(theme);
+    localStorage.setItem("theme", theme);
+  };
+  React.useEffect(() => {
+    const storedTheme = localStorage.getItem("theme");
+    if (storedTheme) {
+      setTheme(storedTheme);
+    }
+  }, [setTheme]);
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -25,13 +34,13 @@ export function ModeToggle({className}:{className?: string}) {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
+        <DropdownMenuItem onClick={() => setUITheme("light")}>
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
+        <DropdownMenuItem onClick={() => setUITheme("dark")}>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
+        <DropdownMenuItem onClick={() => setUITheme("system")}>
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
