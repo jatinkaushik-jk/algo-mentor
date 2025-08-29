03de6c771412b560e34a0234319169e659ca99b5
Jatin Kaushik <jkprogrammer25@gmail.com>
2025-08-29 11:20:21 +0530

    restrict subscription based access to post creation

--- src/app/(overview)/community/page.tsx
+++ src/app/(overview)/community/page.tsx
@@ -4,20 +4,33 @@ import PostCard from "./components/PostCard";
 import CategoryTabs from "./components/CategoryTabs";
 import Link from "next/link";
 import { Post, samplePosts } from "@/lib/sample-post-data";
-import { PlusCircleIcon } from "lucide-react";
+import { LockKeyholeIcon, PlusCircleIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useUserContext } from "@/context/UserProvider";
 
 
 export default function CommunityPage() {
 //   const [posts, setPosts] = useState<Post[]>(samplePosts);
   const [category, setCategory] = useState("For You");
   const [currPosts, setCurrPosts] = useState<Post[]>([]);
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const { getSubscription } = useUserContext();
 
   useEffect(() => {
-    // Simulated API fetch based on category
+    // API fetch based on category
     const filteredPosts = samplePosts.filter(post => post.category === category);
     setCurrPosts(filteredPosts);
-    console.log("Filtered Posts:", filteredPosts);
   }, [category]);
+
+  useEffect(()=>{
+    // API fetch for subscription status
+    const fetchSubscriptionStatus = async () => {
+      const subscription = await getSubscription();
+      setIsSubscribed(()=> subscription?.plan.name !== "Basic");
+    };
+    fetchSubscriptionStatus();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   //   useEffect(() => {
 //     // Simulated API fetch based on category
 //     fetch(`/api/discuss?category=${category}`)
@@ -28,13 +41,16 @@ export default function CommunityPage() {
   return (
     <div>
       <div className="flex justify-end items-center mb-4 px-4">
-        <Link
-          href="/community/create"
-          target="_blank"
-          className="bg-green-500 text-white px-4 py-2 rounded flex items-center gap-2 hover:bg-green-600 transition-colors"
+        <Button asChild
+          className="bg-green-500 hover:bg-green-600 transition-colors flex items-center gap-2"
         >
-          <PlusCircleIcon/> Create
+        <Link
+          href={isSubscribed ? "/community/create" : "/pricing"}
+          target="_blank"
+        >
+          {isSubscribed ? <PlusCircleIcon size={16} /> : <LockKeyholeIcon size={16} />} Create
         </Link>
+        </Button>
       </div>
       <CategoryTabs onChange={(val) => setCategory(val)} />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
