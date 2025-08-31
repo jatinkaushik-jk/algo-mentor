63e2249b0e3d86afcd3dde9bbc060eb7faa50273
Jatin Kaushik <jkprogrammer25@gmail.com>
2025-08-31 13:03:02 +0530

    add algorithm table

--- src/app/algorithms/easy/page.tsx
+++ src/app/algorithms/easy/page.tsx
@@ -1,8 +1,23 @@
-import React from 'react'
+"use client";
+import { useEffect, useState } from "react";
+import AlgorithmTable from "@/components/algorithms/ui/algorithm-table";
+import { AccessValues, Algorithm, DifficultyValues } from "@/models/user.model";
+import { algorithms } from "@/components/algorithms/data/algorithms";
 
 const EasyAlgorithmsPage = () => {
+    const [algos, setAlgos] = useState<Algorithm[]>([]);
+  useEffect(()=>{
+      const fetchAlgorithms = async ()=>{
+        const easyAlgos = algorithms.filter(algo => algo.difficulty === "Easy").map(algo => ({...algo, difficulty: algo.difficulty as DifficultyValues, access: (algo.access as AccessValues)}));
+        setAlgos(easyAlgos);
+      }
+      fetchAlgorithms();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
   return (
-    <div>EasyAlgorithmsPage</div>
+    <div>
+      <AlgorithmTable algos={algos} tableFilterProps={{ difficulty: false }} />
+    </div>
   )
 }
 
