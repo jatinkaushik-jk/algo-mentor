982318f90ca22f42947e406d6843af54fb8ebece
Jatin Kaushik <jkprogrammer25@gmail.com>
2025-12-02 17:31:46 +0530

    update middleware redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,7 @@ import { NextResponse } from "next/server";
 import { auth } from "./app/api/auth/auth";
 
 const secret = process.env.AUTH_SECRET;
+const authPages = ["/login", "/signup", "/forgot-password"];
 const publicRoutes = [
   "/",
   "/pricing",
@@ -12,8 +13,7 @@ const publicRoutes = [
   "/terms-of-service",
   "/pricing",
   "/contact",
-  "/login",
-  "/signup",
+  ...authPages
 ];
 
 export default auth(async function middleware(req:NextRequest) {
@@ -24,21 +24,20 @@ export default auth(async function middleware(req:NextRequest) {
     const isPublic = publicRoutes.some((path) => req.nextUrl.pathname === path);
     if (!isPublic) {
       const newUrl = new URL("/login", req.nextUrl.origin);
-      return NextResponse.rewrite(newUrl);
+      return NextResponse.redirect(newUrl);
     }
   } else {
-    const authPages = ["/login", "/signup", "/forgot-password"];
     if (
       authPages.some((path) => req.nextUrl.pathname.startsWith(path)) &&
       isAuthenticated
     ) {
       // If the user is authenticated and tries to access an auth page, redirect them to the dashboard page
-      return NextResponse.rewrite(new URL("/dashboard", req.nextUrl.origin));
+      return NextResponse.redirect(new URL("/dashboard", req.nextUrl.origin));
     }
     if (isAuthenticated && req.nextUrl.pathname === "/community/create") {
       // If the user is authenticated and tries to access the community's create post page, check for access
       if (token?.subscriptionPlan === "Basic") {
-        return NextResponse.rewrite(new URL("/pricing", req.nextUrl.origin));
+        return NextResponse.redirect(new URL("/pricing", req.nextUrl.origin));
       }
     }
     return NextResponse.next();
